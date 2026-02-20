4d72695b46ab2044295343716393b99369e6cbf3
Umar <umarfarukhshah993@gmail.com>
2026-02-20 09:11:27 +0530

    index.js // index.js console.log("Welcome to Phone Wala!");

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log('Hello, World!');
+// This is a simple JavaScript file that logs "Hello, World!" to the console. You can run this code in a JavaScript environment, such as a web browser's developer console or Node.js, to see the output.
+
+alert('Welcome to Phone Wala!');
+// This line of code will display an alert box with the message "Welcome to Phone Wala!" when the page is loaded. You can customize the message as needed.
+function showMessage() {
+    alert('This is a message from the showMessage function!');
+}
+// This function, when called, will display an alert box with the message "This is a message from the showMessage function!" You can call this function from your HTML, for example, by adding an onclick event to a button.
+
+function changeBackgroundColor(color) {
+    document.body.style.backgroundColor = color;
+}   
+// This function changes the background color of the webpage to the specified color. You can call this function with a color value, such as "red", "blue", or "#00FF00", to change the background color accordingly. For example, you could add a button that calls this function with a specific color when clicked.
